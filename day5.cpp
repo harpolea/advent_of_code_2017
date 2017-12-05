95a4067bd9461976501cac5048a5e91ea806b261
Alice Harpole <ah1e14@soton.ac.uk>
2017-12-05 09:24:45 +0000

    Day 5 solutions

---
+++ day5.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iterator>
+#include <cmath>
+
+using namespace std;
+
+auto read_input(string filename) {
+    vector <int> jumps;
+    ifstream file(filename);
+    copy(istream_iterator<int>(file), istream_iterator<int>(), back_inserter(jumps));
+    return jumps;
+}
+
+auto jump(vector<int> jumps, bool part2) {
+    int count = 0;
+    int position = 0;
+    while (position < jumps.size()) {
+        int j = jumps[position];
+
+        // if part2 this variable will be set
+        if (part2 && j >= 3) {
+            jumps[position]--;
+        } else {
+            jumps[position]++;
+        }
+        position += j;
+        count++;
+    }
+
+    return count;
+}
+
+int main() {
+    string filename = "day5.txt";
+
+    vector <int> jumps = read_input("day5.txt");
+
+    cout << "number of jumps = " << jump(jumps, true) << '\n';
+}
